9fd3a6baa87dfbb3bf88b2465090b885dd325687
Frode <frode@fintlabs.no>
2020-02-08 18:17:11 +0100

    fixed issue with customer list not being unique

--- src/data/redux/actions/customers.js
+++ src/data/redux/actions/customers.js
@@ -1,35 +1,37 @@
+import _ from 'lodash';
 import {
-  FETCH_CUSTOMERS,
-  FETCH_CUSTOMERS_FULFILLED,
-  FETCH_CUSTOMERS_REJECTED,
-  UPDATE_CUSTOMER_NAMES_SPLIT,
+    FETCH_CUSTOMERS,
+    FETCH_CUSTOMERS_FULFILLED,
+    FETCH_CUSTOMERS_REJECTED,
+    UPDATE_CUSTOMER_NAMES_SPLIT,
 } from './actions';
 import GroupRepository from '../../repository/GroupRepository';
 
 
 export default function fetchCustomer(orgId, schoolOrgId) {
-  return (dispatch) => {
-    dispatch({ type: FETCH_CUSTOMERS });
+    return (dispatch) => {
+        dispatch({ type: FETCH_CUSTOMERS });
 
 
-    GroupRepository.fetchAllCustomersFromSchool(orgId, schoolOrgId)
-      .then(([result, json]) => {
-        if (result.status === 200) {
-          dispatch({
-            type: FETCH_CUSTOMERS_FULFILLED,
-            payload: json.customers,
-          });
-          dispatch({
-            type: UPDATE_CUSTOMER_NAMES_SPLIT,
-            payload: json.customers,
-          });
-        }
-      })
-      .catch((error) => {
-        dispatch({
-          type: FETCH_CUSTOMERS_REJECTED,
-          payload: error,
-        });
-      });
-  };
+        GroupRepository.fetchAllCustomersFromSchool(orgId, schoolOrgId)
+            .then(([result, json]) => {
+                if (result.status === 200) {
+                    const customers = _.uniqBy(json.customers, (c) => c.id);
+                    dispatch({
+                        type: FETCH_CUSTOMERS_FULFILLED,
+                        payload: customers,
+                    });
+                    dispatch({
+                        type: UPDATE_CUSTOMER_NAMES_SPLIT,
+                        payload: customers,
+                    });
+                }
+            })
+            .catch((error) => {
+                dispatch({
+                    type: FETCH_CUSTOMERS_REJECTED,
+                    payload: error,
+                });
+            });
+    };
 }
--- src/data/repository/GroupRepository.js
+++ src/data/repository/GroupRepository.js
@@ -1,65 +1,65 @@
 class GroupRepository {
-  static fetchAllCustomersFromSchool(orgId, schoolOrgId) {
-    const url = '/api/group/school';
-    const stopFetch = 15;
-    return fetch(url, {
-      retryOn(attempt, error, response) {
-        return (error !== null || response.status >= 400) && attempt <= stopFetch;
-      },
-      method: 'GET',
-      headers: new Headers({
-        // 'ORG_ID': orgId,
-        SCHOOL_ORG_ID: schoolOrgId,
-      }),
-    })
-      .then((result) => Promise.all([result, result.json()]))
-      .catch((error) => error);
-  }
+    static fetchAllCustomersFromSchool(orgId, schoolOrgId) {
+        const url = '/api/group/school';
+        const stopFetch = 15;
+        return fetch(url, {
+            retryOn(attempt, error, response) {
+                return (error !== null || response.status >= 400) && attempt <= stopFetch;
+            },
+            method: 'GET',
+            headers: new Headers({
+                // 'ORG_ID': orgId,
+                SCHOOL_ORG_ID: schoolOrgId,
+            }),
+        })
+            .then((result) => Promise.all([result, result.json()]))
+            .catch((error) => error);
+    }
 
-  static fetchAllBasisGroupsFromSchool(orgId, schoolOrgId) {
-    const url = '/api/group/basis-group';
-    const stopFetch = 15;
-    return fetch(url, {
-      retryOn(attempt, error, response) {
-        return (error !== null || response.status >= 400) && attempt <= stopFetch;
-      },
-      method: 'GET',
-      headers: new Headers({
-        // 'ORG_ID': orgId,
-        SCHOOL_ORG_ID: schoolOrgId,
-      }),
-    })
-      .then((result) => Promise.all([result, result.json()]))
-      .catch((error) => error);
-  }
+    static fetchAllBasisGroupsFromSchool(orgId, schoolOrgId) {
+        const url = '/api/group/basis-group';
+        const stopFetch = 15;
+        return fetch(url, {
+            retryOn(attempt, error, response) {
+                return (error !== null || response.status >= 400) && attempt <= stopFetch;
+            },
+            method: 'GET',
+            headers: new Headers({
+                // 'ORG_ID': orgId,
+                SCHOOL_ORG_ID: schoolOrgId,
+            }),
+        })
+            .then((result) => Promise.all([result, result.json()]))
+            .catch((error) => error);
+    }
 
-  static fetchAllCustomerGroupsFromTeachingGroupAndSchool() {
-    const url = '/api/group/teaching-group';
-    const stopFetch = 15;
-    return fetch(url, {
-      retryOn(attempt, error, response) {
-        return (error !== null || response.status >= 400) && attempt <= stopFetch;
-      },
-      method: 'GET',
-      // headers: new Headers({ 'x-org-id': orgId })
-    })
-      .then((result) => Promise.all([result, result.json()]))
-      .catch((error) => error);
-  }
+    static fetchAllCustomerGroupsFromTeachingGroupAndSchool() {
+        const url = '/api/group/teaching-group';
+        const stopFetch = 15;
+        return fetch(url, {
+            retryOn(attempt, error, response) {
+                return (error !== null || response.status >= 400) && attempt <= stopFetch;
+            },
+            method: 'GET',
+            // headers: new Headers({ 'x-org-id': orgId })
+        })
+            .then((result) => Promise.all([result, result.json()]))
+            .catch((error) => error);
+    }
 
-  static fetchAllCustomerFromContactTeachingGroupAndSchool() {
-    const url = '/api/group/contact-teacher-group';
-    const stopFetch = 15;
-    return fetch(url, {
-      retryOn(attempt, error, response) {
-        return (error !== null || response.status >= 400) && attempt <= stopFetch;
-      },
-      method: 'GET',
-      // headers: new Headers({ 'x-org-id': orgId })
-    })
-      .then((result) => Promise.all([result, result.json()]))
-      .catch((error) => error);
-  }
+    static fetchAllCustomerFromContactTeachingGroupAndSchool() {
+        const url = '/api/group/contact-teacher-group';
+        const stopFetch = 15;
+        return fetch(url, {
+            retryOn(attempt, error, response) {
+                return (error !== null || response.status >= 400) && attempt <= stopFetch;
+            },
+            method: 'GET',
+            // headers: new Headers({ 'x-org-id': orgId })
+        })
+            .then((result) => Promise.all([result, result.json()]))
+            .catch((error) => error);
+    }
 }
 
 export default GroupRepository;
