3811806a83895474e26f8432fea9641fc2fdc022
OlaStalberg <olastaalberg@gmail.com>
2020-09-10 15:53:49 +0200

    added uploading file posibility, waiting for backend

--- src/feature/payment/step/pick-recipients/pick-recipient.jsx
+++ src/feature/payment/step/pick-recipients/pick-recipient.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {makeStyles, Typography} from '@material-ui/core';
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -18,6 +18,16 @@ import RecipientChipList from '../recipient-chip-list';
 import fetchGroup from '../../../../data/redux/actions/groups';
 import fetchCustomer from '../../../../data/redux/actions/customers';
 import fetchPrincipal from '../../../../data/redux/actions/principal';
+import {useDropzone} from "react-dropzone";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import File from "@material-ui/icons/CloudUpload"
 
 const useStyles = makeStyles(() => ({
     h2: {
@@ -35,6 +45,8 @@ const PickPaymentRecipient = () => {
     const groups = useSelector((state) => state.groups.groups);
     const individual = useSelector((state) => state.customers.customers);
     const schoolOrgId = useSelector((state) => state.payment.payment.schoolOrgId);
+    const [fileAlertOpen, setFileAlertOpen] = useState(false);
+    const [uploadedFiles, setUploadedFiles] = useState([]);
 
 
     useEffect(() => {
@@ -52,13 +64,50 @@ const PickPaymentRecipient = () => {
         dispatch(updateSearchPage(SEARCH_PAGE_START));
     }
 
+    function MyDropzone() {
+        const onDrop = useCallback((acceptedFiles) => {
+            if (acceptedFiles.length > 1) {
+                setUploadedFiles(acceptedFiles);
+                setFileAlertOpen(true);
+            } else {
+                acceptedFiles.forEach((file) => {
+                    if (file.type === "text/csv") {
+
+                    } else {
+
+                    }
+                })
+            }
+
+        }, []);
+        const {getRootProps, getInputProps} = useDropzone({onDrop});
+
+        return (
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <Typography>Last opp liste. Dra en fil hit</Typography>
+            </div>
+        );
+    };
+
+    function handleClose() {
+        setFileAlertOpen(false);
+    }
+
+    function sendToBackend(file) {
+        console.log("sending to backend!");
+        setFileAlertOpen(false);
+    }
 
     return (
         <Box width="90%" mt={4}>
             <Typography variant="h3" className={classes.h2}>Velg mottaker</Typography>
-            <RecipientChipList />
+            <RecipientChipList/>
+            <Box m={2} ml={"auto"} p={1.5} width={150} border={2} borderRadius={50} borderColor={"secondary.main"}>
+                <MyDropzone/>
+            </Box>
             <Box mt={4}>
-                <FormControl component="fieldset" fullWidth >
+                <FormControl component="fieldset" fullWidth>
                     <RadioGroup
                         className={classes.radioGroup}
                         aria-label="recipientType"
@@ -68,20 +117,35 @@ const PickPaymentRecipient = () => {
                     >
                         <FormControlLabel
                             value={GROUP.toString()}
-                            control={<Radio />}
+                            control={<Radio/>}
                             label="Gruppe"
                         />
                         <FormControlLabel
                             value={INDIVIDUAL.toString()}
-                            control={<Radio />}
+                            control={<Radio/>}
                             label="Person"
                         />
                     </RadioGroup>
                     <Box mt={2}>
-                    <RecipientSearch />
+                        <RecipientSearch/>
 
                     </Box>
                 </FormControl>
+                <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={fileAlertOpen}>
+                    <DialogTitle id="simple-dialog-title">Bare mulig å laste opp 1 fil. Velg en</DialogTitle>
+                    <List>
+                        {uploadedFiles ? uploadedFiles.map((file) => (
+                            <ListItem button onClick={() => sendToBackend(file)} key={file.name}>
+                                <ListItemAvatar>
+                                    <Avatar className={classes.avatar}>
+                                        <File/>
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary={file.name}/>
+                            </ListItem>
+                        )):<></>}
+                    </List>
+                </Dialog>
             </Box>
         </Box>
     );
