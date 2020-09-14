2aac04018bd3b1490414ea6a61dc06417b33a4b0
OlaStalberg <olastaalberg@gmail.com>
2020-09-14 14:13:08 +0200

    added back button to orderview and set confirmation on delete all from product and customer list

--- src/common/chips-list-container.jsx
+++ src/common/chips-list-container.jsx
@@ -1,81 +1,101 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Typography} from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import ClearIcon from '@material-ui/icons/Cancel';
 import {makeStyles} from "@material-ui/core/styles";
 import {useSelector} from "react-redux";
+import ConfirmRemoveIcon from '@material-ui/icons/DeleteForever';
+import RemoveIcon from '@material-ui/icons/Delete';
+import PropTypes from 'prop-types'
+
 
 const useStyles = makeStyles(() => ({
     removeAllBox: {
-        display:"flex",
+        display: "flex",
         flexDirection: "end",
         marginInlineStart: "auto",
-        alignItems:"center",
+        alignItems: "center",
+    },
+    toDeleteBox: {
+        display: "flex",
+    }, removeIcon: {
+        cursor: "pointer",
+        verticalAlign:"bottom"
+    },
+    removeIconRed: {
+        color: "red",
+        cursor: "pointer",
+        verticalAlign:"bottom"
     },
 }));
 
-
 const ChipsListContainer = ({
-                                children, content, show, showAllItems, showAllItemsLabel, showLessItemsLabel, toggleShowAllItems, onClear, count, title,
+                                children, content, show, onClear, count, title,
                             }) => {
     const classes = useStyles();
     const step = useSelector(state => state.payment.form.step).toString();
+    const [deleteConfirmed, setDeleteConfirmed] = useState(false);
+    let [timer, setTimer] = React.useState(null);
     const deleteText = "Fjern alle";
-        return (
+    return (
+        <Box
+            visibility={!show ? "hidden" : "visible"}
+            display="flex"
+            justifyContent="center"
+            p={2}
+            flexDirection="column"
+            alignItems="center"
+            flexWrap="flex"
+            width="100%"
+            mt={2}
+            mb={2}
+            height={content === "recipient" ? 220 : 160}
+            border={1}
+            borderColor="grey.100"
+            bgcolor="grey.100"
+            borderRadius="borderRadius"
+        >
             <Box
-                visibility={!show ? "hidden": "visible"}
+                m={1}
                 display="flex"
-                justifyContent="center"
-                p={2}
-                flexDirection="column"
-                alignItems="center"
-                flexWrap="flex"
+                flexDirection="row"
                 width="100%"
-                mt={2}
-                mb={2}
-                height={content ==="recipient" ? 220 : 160}
-                border={1}
-                borderColor="grey.100"
-                bgcolor="grey.100"
-                borderRadius="borderRadius"
+                justifyContent="space-between"
             >
-                <Box
-                    m={1}
-                    display="flex"
-                    flexDirection="row"
-                    width="100%"
-                    justifyContent="space-between"
-                >
-                    <Typography variant="h6">{title} ({count})</Typography>
-                    <Box> {
-                        // content !== "recipient" ?
-                        //     toggleShowAllItems
-                        //     && (
-                        //         <>
-                        //             <Button size="small" onClick={toggleShowAllItems}>
-                        //                 {showAllItems
-                        //                     ? showLessItemsLabel
-                        //                     : showAllItemsLabel}
-                        //             </Button>
-                        //             <Box mr={1} ml={1}>
-                        //                 |
-                        //             </Box>
-                        //         </>
-                        //     )
-                        //     :
-                        (step !== "1" || (step === "1" && content !== "recipient") ) && <Typography variant="caption" pl={1}>{deleteText}</Typography>}
-                        {(step !== "1" || content !== "recipient") && < IconButton size="small" onClick={onClear}>
-                            <ClearIcon/>
-                        </IconButton>}
-                    </Box>
+                <Typography variant="h6">{title} ({count})</Typography>
+                <Box mt={1} mr={"22px"}> {
+                    ((step !== "1" || (step === "1" && content !== "recipient")) && !deleteConfirmed) &&
+                    <Typography variant="caption" pl={1}>{deleteText}</Typography>}
+                    {(step !== "1" || content !== "recipient") ?
+                    deleteConfirmed ?
+                        <div>
+                            <Typography variant={"caption"}>Bekreft</Typography>
+                            <ConfirmRemoveIcon
+                                className={classes.removeIconRed}
+                                onClick={onClear}
 
+                            />
+                        </div> :
+
+                        <RemoveIcon
+                            className={classes.removeIcon}
+                            onClick={() => {
+                                setDeleteConfirmed(true);
+                                clearTimeout(timer);
+                                setTimer(
+                                    setTimeout(function () {
+                                        setDeleteConfirmed(false);
+                                    }, 1500)
+                                );
+                            }}
+                        />: <></>
+                    }
                 </Box>
-                <Box display="flex" width="100%" justifyContent="center" flexWrap="wrap" m={1}>
-                    {children}
-                </Box>
+
             </Box>
-        );
+            <Box display="flex" width="100%" justifyContent="center" flexWrap="wrap" m={1}>
+                {children}
+            </Box>
+        </Box>
+    );
 };
 
 
@@ -84,16 +104,7 @@ ChipsListContainer.propTypes = {
     count: PropTypes.number.isRequired,
     onClear: PropTypes.func.isRequired,
     show: PropTypes.bool.isRequired,
-    showAllItems: PropTypes.bool.isRequired,
-    showAllItemsLabel: PropTypes.string,
-    showLessItemsLabel: PropTypes.string,
     title: PropTypes.string.isRequired,
-    toggleShowAllItems: PropTypes.func,
 };
 
-ChipsListContainer.defaultProps = {
-    showAllItemsLabel: 'Vis alle',
-    showLessItemsLabel: 'Vis færre',
-    toggleShowAllItems: null,
-};
 export default ChipsListContainer;
