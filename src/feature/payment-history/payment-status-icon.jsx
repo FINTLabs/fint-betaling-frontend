67dc70cf7d9feb2deefeed47feeef822139def04
OlaStalberg <olastaalberg@gmail.com>
2020-10-02 09:16:45 +0200

    added icon and text status to order history for cancelled claims

--- src/feature/payment-history/payment-status-icon.jsx
+++ src/feature/payment-history/payment-status-icon.jsx
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {
-    CheckCircle, PaymentRounded, Info, Warning,
-} from '@material-ui/icons';
-import { makeStyles, Typography } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { updateOrderStatusContent, updateOrderStatusOpen } from '../../data/redux/actions/payment';
+import {CheckCircle, Info, PaymentRounded, Warning, Block} from '@material-ui/icons';
+import {makeStyles, Typography} from '@material-ui/core';
+import {useDispatch} from 'react-redux';
+import {updateOrderStatusContent, updateOrderStatusOpen} from '../../data/redux/actions/payment';
 
 const useStyles = makeStyles((theme) => ({
     payedIcon: {
@@ -32,13 +30,19 @@ const useStyles = makeStyles((theme) => ({
         height: '35px',
         verticalAlign: 'text-top',
     },
+    cancelledIcon: {
+        color: theme.status.danger,
+        width: '35px',
+        height: '35px',
+        verticalAlign: 'text-top',
+    },
     statusText: {
         marginLeft: theme.spacing(1),
         alignSelf: 'center',
     },
 }));
 
-const PaymentStatusIcon = ({ payment }) => {
+const PaymentStatusIcon = ({payment}) => {
     const classes = useStyles();
     const dispatch = useDispatch();
 
@@ -61,85 +65,91 @@ const PaymentStatusIcon = ({ payment }) => {
     }
 
     switch (payment.claimStatus) {
-    case 'STORED':
-        paymentIcon = (
-            <Info
-                className={classes.priorityIcon}
-                onClick={(e) => handleStatusClick(e, paymentNotSentFeedback)}
-            />
-        );
-        statusText = <Typography variant="body2" className={classes.statusText}>Lagret, ikke fakturert</Typography>;
-        break;
-    case 'UPDATE_ERROR':
-        paymentIcon = (
-            <Warning
-                className={classes.warningIcon}
-                onClick={(e) => handleStatusClick(e, payment.statusMessage)}
-            />
-        );
-        statusText = <Typography variant="body2" className={classes.statusText}>Feil ved oppdatering</Typography>;
-        break;
-    case 'SEND_ERROR':
-        paymentIcon = (
-            <Warning
-                className={classes.warningIcon}
-                onClick={(e) => handleStatusClick(e, payment.statusMessage)}
-            />
-        );
-        statusText = <Typography variant="body2" className={classes.statusText}>Feil ved innsendelse</Typography>;
-        break;
-    case 'ERROR':
-        paymentIcon = (
-            <Warning
-                className={classes.warningIcon}
-                onClick={(e) => handleStatusClick(e, payment.statusMessage)}
-            />
-        );
-        statusText = <Typography variant="body2" className={classes.statusText}>Ukjent feil</Typography>;
-        break;
-    case 'ACCEPT_ERROR':
-        paymentIcon = (
-            <Warning
-                className={classes.warningIcon}
-                onClick={(e) => handleStatusClick(e, payment.statusMessage)}
-            />
-        );
-        statusText = (
-            <Typography variant="body2" className={classes.statusText}>
-                Feil ved oversending til økonomisystemet
-            </Typography>
-        );
-        break;
-    case 'PAID':
-        paymentIcon = <CheckCircle className={classes.payedIcon} />;
-        statusText = <Typography variant="body2" className={classes.statusText}>Betalt</Typography>;
-        break;
-    case 'ACCEPTED':
-        paymentIcon = <PaymentRounded className={classes.waitingPaymentIcon} />;
-        statusText = <Typography variant="body2" className={classes.statusText}>Klar til fakturering</Typography>;
-        break;
-    case 'SENT':
-        paymentIcon = <PaymentRounded className={classes.waitingPaymentIcon} />;
-        statusText = <Typography variant="body2" className={classes.statusText}>Sendt til økonomisystem</Typography>;
-        break;
-    case 'ISSUED':
-        paymentIcon = <PaymentRounded className={classes.waitingPaymentIcon} />;
-        statusText = <Typography variant="body2" className={classes.statusText}>Fakturert</Typography>;
-        break;
+        case 'STORED':
+            paymentIcon = (
+                <Info
+                    className={classes.priorityIcon}
+                    onClick={(e) => handleStatusClick(e, paymentNotSentFeedback)}
+                />
+            );
+            statusText = <Typography variant="body2" className={classes.statusText}>Lagret, ikke fakturert</Typography>;
+            break;
+        case 'UPDATE_ERROR':
+            paymentIcon = (
+                <Warning
+                    className={classes.warningIcon}
+                    onClick={(e) => handleStatusClick(e, payment.statusMessage)}
+                />
+            );
+            statusText = <Typography variant="body2" className={classes.statusText}>Feil ved oppdatering</Typography>;
+            break;
+        case 'SEND_ERROR':
+            paymentIcon = (
+                <Warning
+                    className={classes.warningIcon}
+                    onClick={(e) => handleStatusClick(e, payment.statusMessage)}
+                />
+            );
+            statusText = <Typography variant="body2" className={classes.statusText}>Feil ved innsendelse</Typography>;
+            break;
+        case 'ERROR':
+            paymentIcon = (
+                <Warning
+                    className={classes.warningIcon}
+                    onClick={(e) => handleStatusClick(e, payment.statusMessage)}
+                />
+            );
+            statusText = <Typography variant="body2" className={classes.statusText}>Ukjent feil</Typography>;
+            break;
+        case 'ACCEPT_ERROR':
+            paymentIcon = (
+                <Warning
+                    className={classes.warningIcon}
+                    onClick={(e) => handleStatusClick(e, payment.statusMessage)}
+                />
+            );
+            statusText = (
+                <Typography variant="body2" className={classes.statusText}>
+                    Feil ved oversending til økonomisystemet
+                </Typography>
+            );
+            break;
+        case 'PAID':
+            paymentIcon = <CheckCircle className={classes.payedIcon}/>;
+            statusText = <Typography variant="body2" className={classes.statusText}>Betalt</Typography>;
+            break;
+        case 'ACCEPTED':
+            paymentIcon = <PaymentRounded className={classes.waitingPaymentIcon}/>;
+            statusText = <Typography variant="body2" className={classes.statusText}>Klar til fakturering</Typography>;
+            break;
+        case 'SENT':
+            paymentIcon = <PaymentRounded className={classes.waitingPaymentIcon}/>;
+            statusText =
+                <Typography variant="body2" className={classes.statusText}>Sendt til økonomisystem</Typography>;
+            break;
+        case 'CANCELLED':
+            paymentIcon = <Block className={classes.cancelledIcon}/>;
+            statusText =
+                <Typography variant="body2" className={classes.statusText}>Kansellert før fakturering</Typography>;
+            break;
+        case 'ISSUED':
+            paymentIcon = <PaymentRounded className={classes.waitingPaymentIcon}/>;
+            statusText = <Typography variant="body2" className={classes.statusText}>Fakturert</Typography>;
+            break;
         default:
-        paymentIcon = (
-            <Warning
-                className={classes.warningIcon}
-                onClick={(e) => handleStatusClick(e, payment.error)}
-            />
-        );
-        statusText = (
-            <Typography variant="body2" className={classes.statusText}>
-                Klarte ikke finne
-                ordrestatus
-            </Typography>
-        );
-        break;
+            paymentIcon = (
+                <Warning
+                    className={classes.warningIcon}
+                    onClick={(e) => handleStatusClick(e, payment.error)}
+                />
+            );
+            statusText = (
+                <Typography variant="body2" className={classes.statusText}>
+                    Klarte ikke finne
+                    ordrestatus
+                </Typography>
+            );
+            break;
     }
     return (
         <>
