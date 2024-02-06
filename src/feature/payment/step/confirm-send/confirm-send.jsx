af17489cf8b8f5dc9ee06fce3e6aaf7208b5c38b
jennifer-fintlabs <jennifer@fintlabs.no>
2024-02-06 14:54:05 +0100

    changed json layout of order line to match the new backend data layout

--- src/feature/payment/step/confirm-send/confirm-send.jsx
+++ src/feature/payment/step/confirm-send/confirm-send.jsx
@@ -57,13 +57,13 @@ const ConfirmSend = () => {
                             description: description ? description[key].description : '',
                             itemQuantity: amount[key].amount,
                             itemPrice: price[key].itemPrice,
-                            lineitem: {
-                                itemCode: key,
-                                itemPrice: productsX[key].itemPrice,
-                                taxrate: productsX[key].taxRate,
-                                description: productsX[key].description,
-                                uri: productsX[key].uri,
-                            },
+                            // lineitem: {
+                            itemCode: key,
+                            originalItemPrice: productsX[key].itemPrice,
+                            taxrate: productsX[key].taxRate,
+                            originalDescription: productsX[key].description,
+                            uri: productsX[key].uri,
+                            // },
                         };
                         list.push(orderLine);
                     }
