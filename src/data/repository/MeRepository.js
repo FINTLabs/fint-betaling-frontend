4b2defb577f17043dc0b03ff988abd80a8bad40e
jennifer-fintlabs <jennifer@fintlabs.no>
2024-02-16 13:07:31 +0100

    removed a slash from me :cat:

--- src/data/repository/MeRepository.js
+++ src/data/repository/MeRepository.js
@@ -1,6 +1,6 @@
 class MeRepository {
     static fetchMe() {
-        const url = '/api/me';
+        const url = 'api/me';
         const stopFetch = 15;
         return fetch(url, {
             retryOn(attempt, error, response) {
