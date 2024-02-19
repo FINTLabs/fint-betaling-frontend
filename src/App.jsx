8efb17c1f3ba8bcb04643a98326e3540bc669068
jennifer-fintlabs <jennifer@fintlabs.no>
2024-02-19 15:04:28 +0100

    removed all that stuff frode did and try to do a simple version.... 🐰

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import {
     createTheme,
@@ -6,7 +6,6 @@ import {
 } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import axios from 'axios';
 import Scaffold from './common/scaffold/scaffold';
 import store from './data/redux/store';
 
@@ -52,22 +51,7 @@ const theme = createTheme({
 });
 
 function App() {
-    const [basePath, setBasePath] = useState();
-    useEffect(() => {
-        axios
-            .get('/api/application/configuration')
-            .then((value) => {
-                axios.defaults.baseURL = value.data.basePath;
-                setBasePath(value.data.basePath);
-            })
-            .catch((reason) => {
-                // eslint-disable-next-line no-console
-                console.log(reason);
-                setBasePath('/');
-            });
-        // eslint-disable-next-line no-console
-        console.log('Base path:', basePath);
-    }, [basePath]);
+    const basePath = process.env.BASE_PATH || '/';
 
     return basePath ? (
         <ThemeProvider theme={theme}>
