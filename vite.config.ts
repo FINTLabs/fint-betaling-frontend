55e0da64803eff0d7dfc51ee94ca03438c76f2d5
jennifer-fintlabs <jennifer@fintlabs.no>
2026-02-19 11:23:27 +0100

    set vite server to port 3000 🐰

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    port: 3000,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 3000,
+    },
+  },
 });
