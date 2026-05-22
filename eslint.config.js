a62211ba019bdc8ba62a2a035b27b255469bf246
jennifer-fintlabs <jennifer@fintlabs.no>
2026-05-22 15:50:59 +0200

    upgrade vite to 8 🐰

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,7 @@ export default tseslint.config(
       "node_modules/**",
       ".react-router/**",
       "public/mockServiceWorker.js",
+        "coverage/**",
     ],
   },
   js.configs.recommended,
