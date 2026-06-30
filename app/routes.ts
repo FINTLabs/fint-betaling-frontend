b45a2fc16f1d98e04e84365ee2c488ea5e9a62f2
jennifer-fintlabs <jennifer@fintlabs.no>
2026-06-30 12:05:31 +0200

    deploy to beta only 🐰

--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,5 @@ export default [
   route("send", "routes/claim-pending.tsx"),
   route("ny", "routes/claim-new.tsx"),
   route("historikk", "routes/claim-history.tsx"),
-  route("test", "routes/testAnalytics.tsx"),
   route("_analytics/events", "routes/api.events.tsx"),
 ] satisfies RouteConfig;
